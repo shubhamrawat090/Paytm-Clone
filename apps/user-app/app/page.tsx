20dcdae733daea2c94c26346f8147c18a76699b1
Grudge69 <shubham.shubham.rawat090@gmail.com>
2024-03-25 19:55:36 +0530

    Completes Prisma Setup

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,3 +1,6 @@
+import { PrismaClient } from "@repo/db/client";
+const client = new PrismaClient();
+
 export default function Page(): JSX.Element {
   return <div className="text-2xl">Hi there</div>;
 }
